1d29f130f7adfceafaeb9efba522780285864e6c
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-04-07 12:18:43 +0200

    done express with sql
    got reminded by the whole .env thing
    reminded of how to use sql with express
    the put patch standard checking (route to req body)
    and finally the difference in types between req.body and params

--- server/routes/trees.js
+++ server/routes/trees.js
@@ -1,6 +1,11 @@
 // Instantiate router - DO NOT MODIFY
 const express = require('express');
+require('dotenv').config();
+const sqlite3 = require('sqlite3');
 const router = express.Router();
+
+
+// Now you can access your environment variables like this:
 
 /**
  * BASIC PHASE 2, Step A - Instantiate SQLite and database
@@ -9,18 +14,34 @@ const router = express.Router();
  */
 // Your code here
 
+
+const db = new sqlite3.Database(process.env.DATA_SOURCE, sqlite3.OPEN_READWRITE);
+
 /**
  * BASIC PHASE 2, Step B - List of all trees in the database
  *
  * Protocol: GET
  * Path: /
+ * 
  * Parameters: None
  * Response: JSON array of objects
  *   - Object properties: height-ft, tree, id
  *   - Ordered by the height_ft from tallest to shortest
  */
 // Your code here
-
+router.get("/",(req,res,next)=> {
+    const SqlStatement = `SELECT id,tree FROM trees ORDER BY height_ft DESC`
+    const params = [];
+    db.all(SqlStatement, params, (err,rows)=> {
+        if (err) {
+           
+            next(err);
+            return;
+        }
+       
+        res.json(rows);
+    })
+})
 /**
  * BASIC PHASE 3 - Retrieve one tree with the matching id
  *
@@ -31,6 +52,17 @@ const router = express.Router();
  *   - Properties: id, tree, location, height_ft, ground_circumference_ft
  */
 // Your code here
+router.get("/:id",(req,res,next)=> {
+    const SqlStatement = `SELECT * FROM trees Where id=?`
+    const params = [req.params.id];
+    db.get(SqlStatement,params, (err,rows)=> {
+        if(err) {
+            next(err);
+            return;
+        }
+        res.json(rows);
+    })
+})
 
 /**
  * INTERMEDIATE PHASE 4 - INSERT tree row into the database
@@ -43,6 +75,20 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+router.post("/",(req,res,next)=> {
+    const SqlStatement = `INSERT INTO trees(tree,location,height_ft,ground_circumference_ft)
+                                VALUES (?,?,?,?)`;
+    const params = [req.body.name,req.body.location,req.body.height,req.body.size];
+    db.run(SqlStatement,params,(err,rows)=> {
+        if(err) {
+            next(err);
+            return;
+        }
+        res.json({
+            message: "success"
+        });
+    })
+})
 
 /**
  * INTERMEDIATE PHASE 5 - DELETE a tree row from the database
@@ -55,7 +101,19 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
-
+router.delete("/:id",(req,res,next)=> {
+    const SqlStatement = `DELETE FROM trees WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(SqlStatement,params,(err,rows) => {
+        if(err) {
+            next(err);
+            return;
+        }
+        res.json({
+            message: "success"
+        })
+    })
+})
 /**
  * INTERMEDIATE PHASE 6 - UPDATE a tree row in the database
  *
@@ -67,6 +125,32 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+const updateTreeById = (req,res,next)  => {
+   //loose equality because one of them is a string
+    if (req.body.id != req.params.id) {
+       let err = new Error("ids do not match");
+       
+       err.statusCode = 400;
+       next(err);
+       return;
+    }
+    const SqlStatement = `UPDATE trees
+                        SET tree = ?,location = ?,height_ft =?,ground_circumference_ft =?
+                        WHERE id = ?`
+    const params = [req.body.name,req.body.location,req.body.height,req.body.size,req.body.id];
+
+    db.run(SqlStatement,params,(err,rows) => {
+        if(err) {
+            next(err);
+            return;
+        }
+        res.json({
+            message: "success"
+        })
+    })
+}
+router.put("/:id",updateTreeById);
+router.patch("/:id",updateTreeById);
 
 // Export class - DO NOT MODIFY
 module.exports = router;
--- server/seed-data.sql
+++ server/seed-data.sql
@@ -1,5 +1,45 @@
 -- BASIC PHASE 1A - DROP and CREATE table
+DROP TABLE IF EXISTS trees;
+
+CREATE TABLE trees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tree VARCHAR(32),
+    location VARCHAR(64),
+    height_ft NUMERIC,
+    ground_circumference_ft NUMERIC
+);
 -- Your code here
 
 -- BASIC PHASE 1B - INSERT seed data
 -- Your code here
+INSERT INTO trees (tree, location, height_ft, ground_circumference_ft) VALUES 
+('General Sherman', 'Sequoia National Park', 274.9, 102.6),
+('General Grant*', 'Kings Canyon National Park', 268.1, 107.5),
+('President*', 'Sequoia National Park', 240.9, 93),
+('Lincoln', 'Sequoia National Park', 255.8, 98.3),
+('Stagg', 'Alder Creek', 243, 109),
+('Boole', 'Converse Basin', 268.8, 113),
+('Genesis', 'Mountain Home State Forest', 253, 85.3),
+('Franklin', 'Sequoia National Park', 223.8, 94.8),
+('King Arthur', 'Sequoia National Park', 270.3, 104.2),
+('Monroe', 'Sequoia National Park', 247.8, 91.3),
+('Robert E. Lee', 'Kings Canyon National Park', 254.7, 88.3),
+('unnamed', 'Garfield Grove', 273.1, 99.5),
+('Adams', 'Sequoia National Park', 250.6, 83.3),
+('Ishi Giant', 'Giant Sequoia National Monument', 255, 105.1),
+('Column', 'Sequoia National Park', 243.8, 93),
+('Summit Road', 'Mountain Home State Forest', 244, 82.2),
+('Euclid', 'Mountain Home State Forest', 272.7, 83.4),
+('Washington', 'Yosemite National Park', 236, 95.7),
+('Pershing', 'Sequoia National Park', 246, 91.2),
+('Diamond', 'Sequoia National Park', 286, 95.3),
+('Adams', 'Mountain Home State Forest', 247.4, 94.2),
+('Roosevelt/False Heart', 'Kings Canyon National Park', 260, 80),
+('Nelder', 'Sierra National Forest', 266.2, 90),
+('AD', 'Sequoia National Park', 242.4, 99),
+('Hart', 'Kings Canyon National Park', 277.9, 75.3),
+('Grizzly Giant', 'Yosemite National Park', 209, 92.5),
+('Chief Sequoyah', 'Sequoia National Park', 228.2, 90.4),
+('Methuselah', 'Mountain Home State Forest', 207.8, 95.8),
+('Great Goshawk', 'Giant Sequoia National Monument', 255.2, 90.2),
+('Hamilton', 'Sequoia National Park', 238.5, 82.6);
